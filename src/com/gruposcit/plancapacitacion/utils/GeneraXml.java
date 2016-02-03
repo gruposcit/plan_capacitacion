13b1e7ed785840c09a9268a744dc81659ae9e0ad
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-02-03 16:19:56 -0700

    Se agregaron los cursos programados, aun incompleto pero ya agrega al menos uno

--- src/com/gruposcit/plancapacitacion/utils/GeneraXml.java
+++ src/com/gruposcit/plancapacitacion/utils/GeneraXml.java
@@ -5,7 +5,10 @@
  */
 package com.gruposcit.plancapacitacion.utils;
 
+import java.awt.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -38,7 +41,7 @@ public class GeneraXml {
 //    }
     
 //    public GeneraXml(String nom_arch, ArrayList<Map> datos) throws ParserConfigurationException{
-    public GeneraXml(String nom_arch, String cad_organo, String cad_sujeto, String cad_periodo) throws ParserConfigurationException, TransformerConfigurationException, TransformerException{
+    public GeneraXml(String nom_arch, String cad_organo, String cad_sujeto, String cad_periodo, ArrayList<Map> actual) throws ParserConfigurationException, TransformerConfigurationException, TransformerException{
 //     if (datos.isEmpty()){
 //         System.out.println("no hay datos para generar el archivo");
 //         return;
@@ -85,19 +88,77 @@ public class GeneraXml {
      Element capacitacion_ano_anterior = document.createElement("capacitacion_ano_anterior");
      datos_capacitacion.appendChild(capacitacion_ano_anterior);
      
+     Element cursos_programados = document.createElement("cursos_programados");
      
-//    System.out.println(document.getTextContent());
+     
+    Element tipo_capacitacion = document.createElement("tipo_capacitacion");
+    Element nombre_capacitacion = document.createElement("nombre_capacitacion");
+    Element fecha_inicio_imparticion = document.createElement("fecha_inicio_imparticion");
+    Element fecha_fin_imparticion = document.createElement("fecha_fin_imparticion");
+    Element areas_capacitacion = document.createElement("areas_capacitacion");
+    Element total_personas = document.createElement("total_personas");
+
+     
+     for (Map dato : actual){
+         System.out.println(dato);
+        tipo_capacitacion.setTextContent(dato.get("tipo").toString());
+        cursos_programados.appendChild(tipo_capacitacion);
+              
+        nombre_capacitacion.setTextContent(dato.get("nombre").toString());
+        cursos_programados.appendChild(nombre_capacitacion);
+              
+        fecha_inicio_imparticion.setTextContent(dato.get("feini").toString());
+        cursos_programados.appendChild(fecha_inicio_imparticion);
+         
+        fecha_fin_imparticion.setTextContent(dato.get("fefin").toString());
+        cursos_programados.appendChild(fecha_fin_imparticion);  
+        
+        areas_capacitacion.setTextContent(dato.get("areas").toString());
+        cursos_programados.appendChild(areas_capacitacion);
+        
+        total_personas.setTextContent(dato.get("num_per").toString());
+        cursos_programados.appendChild(total_personas);
+        
+        programa_anual.appendChild(cursos_programados); 
+        datos_capacitacion.appendChild(programa_anual); 
+     }
+     
+     
     Source source = new DOMSource(document);
     Result result = new StreamResult(new java.io.File(nom_arch+".xml"));
     Transformer transformer = TransformerFactory.newInstance().newTransformer();
     transformer.setOutputProperty(OutputKeys.INDENT, "yes");
     transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-//    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-     transformer.transform(source, result);
+    transformer.transform(source, result);
+    
     }
     
     public static void main(String[] args) throws ParserConfigurationException, TransformerException{
-        GeneraXml gen = new GeneraXml("informe","001-002","89-123","2015");
+        ArrayList<Map> actuales = new ArrayList<Map>();
+        Map reg = reg = new HashMap();
+        
+        reg.put("tipo", "01");
+        reg.put("nombre", "CAPACITATE HOY o que");
+        reg.put("feini", "2016-06");
+        reg.put("fefin", "2016-06");
+        reg.put("areas", "SISTEMAS");
+        reg.put("num_per", "12");
+        actuales.add(reg);
+        
+        Map reg2 = new HashMap();
+        reg2.put("tipo", "01");
+        reg2.put("nombre", "CAPACITATE HOY sale");
+        reg2.put("feini", "2016-06");
+        reg2.put("fefin", "2016-06");
+        reg2.put("areas", "SISTEMAS,CONTABILIDAD");
+        reg2.put("num_per", "12");
+        actuales.add(reg2);
+        
+        
+        System.out.println(actuales.size());
+        System.out.println(actuales);
+        
+        GeneraXml gen = new GeneraXml("informe","001-002","89-123","2015", actuales );
     }
     
 }
