d8d4e81fe5494832893ff3cfdb89a9aa86f26e38
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2016-02-03 17:17:54 -0700

    Genera XML correcto, se válido contra el xsd y pasó la prueba, también se agrega ICC.xsd

--- src/com/gruposcit/plancapacitacion/utils/GeneraXml.java
+++ src/com/gruposcit/plancapacitacion/utils/GeneraXml.java
@@ -41,7 +41,7 @@ public class GeneraXml {
 //    }
     
 //    public GeneraXml(String nom_arch, ArrayList<Map> datos) throws ParserConfigurationException{
-    public GeneraXml(String nom_arch, String cad_organo, String cad_sujeto, String cad_periodo, ArrayList<Map> actual) throws ParserConfigurationException, TransformerConfigurationException, TransformerException{
+    public GeneraXml(String nom_arch, String cad_organo, String cad_sujeto, String cad_periodo, ArrayList<Map> actual, ArrayList<Map> anterior) throws ParserConfigurationException, TransformerConfigurationException, TransformerException{
 //     if (datos.isEmpty()){
 //         System.out.println("no hay datos para generar el archivo");
 //         return;
@@ -88,23 +88,31 @@ public class GeneraXml {
      Element capacitacion_ano_anterior = document.createElement("capacitacion_ano_anterior");
      datos_capacitacion.appendChild(capacitacion_ano_anterior);
      
-     Element cursos_programados = document.createElement("cursos_programados");
+//     Element cursos_programados = document.createElement("cursos_programados");
      
      
-    Element tipo_capacitacion = document.createElement("tipo_capacitacion");
-    Element nombre_capacitacion = document.createElement("nombre_capacitacion");
-    Element fecha_inicio_imparticion = document.createElement("fecha_inicio_imparticion");
-    Element fecha_fin_imparticion = document.createElement("fecha_fin_imparticion");
-    Element areas_capacitacion = document.createElement("areas_capacitacion");
-    Element total_personas = document.createElement("total_personas");
+//    Element tipo_capacitacion = document.createElement("tipo_capacitacion");
+//    Element nombre_capacitacion = document.createElement("nombre_capacitacion");
+//    Element fecha_inicio_imparticion = document.createElement("fecha_inicio_imparticion");
+//    Element fecha_fin_imparticion = document.createElement("fecha_fin_imparticion");
+//    Element areas_capacitacion = document.createElement("areas_capacitacion");
+//    Element total_personas = document.createElement("total_personas");
 
      
      for (Map dato : actual){
-         System.out.println(dato);
+         
+       Element cursos_programados = document.createElement("cursos_programados");
+       Element tipo_capacitacion = document.createElement("tipo_capacitacion");
+       Element nombre_capacitacion = document.createElement("nombre_capacitacion");
+       Element fecha_inicio_imparticion = document.createElement("fecha_inicio_imparticion");
+       Element fecha_fin_imparticion = document.createElement("fecha_fin_imparticion");
+       Element areas_capacitacion = document.createElement("areas_capacitacion");
+       Element total_personas = document.createElement("total_personas");
+
         tipo_capacitacion.setTextContent(dato.get("tipo").toString());
         cursos_programados.appendChild(tipo_capacitacion);
               
-        nombre_capacitacion.setTextContent(dato.get("nombre").toString());
+        nombre_capacitacion.setTextContent(dato.get("nombre").toString().toUpperCase());
         cursos_programados.appendChild(nombre_capacitacion);
               
         fecha_inicio_imparticion.setTextContent(dato.get("feini").toString());
@@ -123,42 +131,84 @@ public class GeneraXml {
         datos_capacitacion.appendChild(programa_anual); 
      }
      
+     for (Map dato : anterior){
+         
+       Element cursos_programados = document.createElement("cursos_efectuados");
+       Element tipo_capacitacion = document.createElement("tipo_capacitacion");
+       Element nombre_capacitacion = document.createElement("nombre_capacitacion");
+       Element fecha_inicio_imparticion = document.createElement("fecha_inicio_imparticion");
+       Element fecha_fin_imparticion = document.createElement("fecha_fin_imparticion");
+       Element areas_capacitacion = document.createElement("areas_capacitadas");
+       Element total_personas = document.createElement("total_personas_capacitadas");
+       Element documento_emitido = document.createElement("documento_emitido");
+
+        tipo_capacitacion.setTextContent(dato.get("tipo").toString());
+        cursos_programados.appendChild(tipo_capacitacion);
+              
+        nombre_capacitacion.setTextContent(dato.get("nombre").toString().toUpperCase());
+        cursos_programados.appendChild(nombre_capacitacion);
+              
+        fecha_inicio_imparticion.setTextContent(dato.get("feini").toString());
+        cursos_programados.appendChild(fecha_inicio_imparticion);
+         
+        fecha_fin_imparticion.setTextContent(dato.get("fefin").toString());
+        cursos_programados.appendChild(fecha_fin_imparticion);  
+        
+        areas_capacitacion.setTextContent(dato.get("areas").toString());
+        cursos_programados.appendChild(areas_capacitacion);
+        
+        total_personas.setTextContent(dato.get("num_per").toString());
+        cursos_programados.appendChild(total_personas);
+        
+        documento_emitido.setTextContent(dato.get("documento").toString().toUpperCase());
+        cursos_programados.appendChild(documento_emitido);
+        
+        
+        capacitacion_ano_anterior.appendChild(cursos_programados); 
+        datos_capacitacion.appendChild(capacitacion_ano_anterior); 
+     }
+     
      
     Source source = new DOMSource(document);
     Result result = new StreamResult(new java.io.File(nom_arch+".xml"));
     Transformer transformer = TransformerFactory.newInstance().newTransformer();
     transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
     transformer.transform(source, result);
     
     }
     
     public static void main(String[] args) throws ParserConfigurationException, TransformerException{
         ArrayList<Map> actuales = new ArrayList<Map>();
-        Map reg = reg = new HashMap();
+        ArrayList<Map> anteriores = new ArrayList<Map>();
+        Map reg;
         
-        reg.put("tipo", "01");
-        reg.put("nombre", "CAPACITATE HOY o que");
-        reg.put("feini", "2016-06");
-        reg.put("fefin", "2016-06");
-        reg.put("areas", "SISTEMAS");
-        reg.put("num_per", "12");
-        actuales.add(reg);
+        for (int x = 0; x<3; x++){
+            reg = new HashMap();
+            reg.put("tipo", "0" +x);
+            reg.put("nombre", "CAPACITATE HOY o que  " + x);
+            reg.put("feini", "2016-06");
+            reg.put("fefin", "2016-06");
+            reg.put("areas", "SISTEMAS");
+            reg.put("num_per", "12" + x);
+            actuales.add(reg);
+        }
         
-        Map reg2 = new HashMap();
-        reg2.put("tipo", "01");
-        reg2.put("nombre", "CAPACITATE HOY sale");
-        reg2.put("feini", "2016-06");
-        reg2.put("fefin", "2016-06");
-        reg2.put("areas", "SISTEMAS,CONTABILIDAD");
-        reg2.put("num_per", "12");
-        actuales.add(reg2);
-        
-        
+         for (int x = 0; x<3; x++){
+            reg = new HashMap();
+            reg.put("tipo", "0" +x);
+            reg.put("nombre", "YA PASO LA CAPACITACION AYER o que  " + x);
+            reg.put("feini", "2016-06");
+            reg.put("fefin", "2016-06");
+            reg.put("areas", "SISTEMAS");
+            reg.put("num_per", "12" + x);
+            reg.put("documento", "CERTIFICADO");
+            anteriores.add(reg);
+        }
         System.out.println(actuales.size());
         System.out.println(actuales);
         
-        GeneraXml gen = new GeneraXml("informe","001-002","89-123","2015", actuales );
+        GeneraXml gen = new GeneraXml("informe","01-002","89-123","2015", actuales, anteriores );
     }
     
 }
